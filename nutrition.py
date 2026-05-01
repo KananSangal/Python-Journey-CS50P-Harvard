3bc2686bf8b784fba279d6bb5cc40c9795e3d0a7
KananSangal <kanansangal@gmail.com>
2026-05-01 16:17:53 +0530

    Add nutrition.py to display calorie information for selected fruits

---
+++ nutrition.py
@@ -0,0 +1,10 @@
+#Week 2, problem set 0, Que 2. 
+#This program will ask the user for a fruit and then print out the calories in that fruit.
+dict = {"apple": 130, "avocado": 50, "banana": 110, "cantaloupe": 50,
+        "grapefruit": 60, "honeydew melon": 50, "kiwifruit": 90, "lemon": 15,
+        "lime": 20, "nectarine": 60, "orange": 80, "peach": 60, "pear": 100,
+        "pineapple": 50, "plums": 70, "strawberries": 50, "sweet cherries": 100,
+        "tangerine": 50, "watermelon": 80}
+user_input = input("Item: ")
+if user_input.lower() in dict:
+    print("Calories: ", dict[user_input.lower()])
