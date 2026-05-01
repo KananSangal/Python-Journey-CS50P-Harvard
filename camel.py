835fd243e75bf3346f3b5f85ba2871cf3316ee65
KananSangal <kanansangal@gmail.com>
2026-05-01 14:20:22 +0530

    Add camel.py to convert camelCase to snake_case

---
+++ camel.py
@@ -0,0 +1,19 @@
+#Week 2, problem set 0. Converting camelCase to snake_case.
+user_input = input("camelCase: ")
+
+def camel_case(s):
+    if s == None or s == "":
+        return ("enter valid input")
+    else:
+        snake = ""
+        for i in s:
+            if i.islower():
+                snake += i
+            else:
+                snake += "_" + i.lower()
+        return snake
+def main():
+    camel = camel_case(user_input)
+    print(camel)
+
+main()
